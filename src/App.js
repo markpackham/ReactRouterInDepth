8bdd018d8266df073b724ba55395af9671321068
mark <markpackham1@gmail.com>
2022-12-15 18:39:37 +0000

    add nav in RootLayout
    https://youtu.be/XN7E5tKM-no?t=428

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { BrowserRouter, Routes, Route, Link } from "read-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// Layouts
+import RootLayout from "./layouts/RootLayout";
 
 function App() {
-  return;
-  <BrowserRouter>
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </main>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<RootLayout />} />
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
