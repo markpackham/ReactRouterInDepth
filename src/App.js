f17aeb87dfd9fedd51bdb441c592591d57d2ff24
mark <markpackham1@gmail.com>
2022-12-15 18:55:06 +0000

    user modern Router and make nav sitewide
    https://youtu.be/5s57C7leXc4?t=455

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 // Layouts
+// RootLayout with surround every page
 import RootLayout from "./layouts/RootLayout";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          <Link to="/">Home</Link>
-          <NavLink to="about">About</NavLink>
-          <NavLink to="help">Help</NavLink>
-          <NavLink to="careers">Careers</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
