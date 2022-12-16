2aba56d7cc3ab357e236e342780c19a4c2cf1147
mark <markpackham1@gmail.com>
2022-12-16 09:25:39 +0000

    update HelpLayout

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ import About from "./pages/About";
 // Layouts
 // RootLayout with surround every page
 import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />} />
     </Route>
   )
 );
