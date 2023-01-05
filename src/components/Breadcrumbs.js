2d80fd39a535d808297d7abcd26f83a8d7edd115
mark <markpackham1@gmail.com>
2023-01-05 09:55:12 +0000

    finish off breadcrumbs

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,11 +1,22 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 export default function Breadcrumbs() {
   const location = useLocation();
 
   let currentLink = "";
 
-  const crumbs = location.pathname.split("/");
+  const crumbs = location.pathname
+    .split("/")
+    .filter((crumb) => crumb !== "")
+    .map((crumb) => {
+      currentLink += `/${crumb}`;
 
-  return <div>Breadcrumbs</div>;
+      return (
+        <div className="crumb" key={crumb}>
+          <Link to={currentLink}>{crumb}</Link>
+        </div>
+      );
+    });
+
+  return <div className="breadcrumbs">{crumbs}</div>;
 }
