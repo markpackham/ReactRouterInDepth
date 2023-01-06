15204e11e4c91b4592028ffe90a4cd0d1407db3d
mark <markpackham1@gmail.com>
2023-01-06 08:51:34 +0000

    simulate logging out and being redirected from a page
    https://youtu.be/JNWSMJshmOQ?t=254

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+
 export default function About() {
+  const [user, setUser] = useState("Edward");
+
+  // we set a user by default so the only way to logout is clicking on the Logout button
+  if (!user) {
+    // "replace" is used to replace the history so the "About" page is removed from it
+    // this will impact our Browser's back button so we can't return to "About" when logged out
+    return <Navigate to="/" replace={true} />;
+  }
+
   return (
     <div className="about">
       <h2>About Us</h2>
@@ -23,6 +35,8 @@ export default function About() {
         quia illum officia obcaecati provident nulla odio molestiae suscipit
         quasi.
       </p>
+
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   );
 }
